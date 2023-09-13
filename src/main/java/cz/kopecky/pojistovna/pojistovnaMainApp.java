5ff8cd202b5a70f29536d16eb0fea6caa656f6ba
Kopecký Jiří <jukopecky@gmail.com>
2023-09-13 21:40:17 +0200

    vytvoření fragmentů + home/index

--- src/main/java/cz/kopecky/pojistovna/pojistovnaMainApp.java
+++ src/main/java/cz/kopecky/pojistovna/pojistovnaMainApp.java
@@ -1,10 +1,12 @@
 package cz.kopecky.pojistovna;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class pojistovnaMainApp {
     public static void main(String[] args) {
+        SpringApplication.run(pojistovnaMainApp.class, args);
 
     }
 }
