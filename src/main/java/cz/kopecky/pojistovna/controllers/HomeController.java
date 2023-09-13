b97778ceb8819f10ae4b599c61ea5cd9c68997ed
Kopecký Jiří <jukopecky@gmail.com>
2023-09-13 21:13:52 +0200

    Vytvoření homeControlleru + šablona index

---
+++ src/main/java/cz/kopecky/pojistovna/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package cz.kopecky.pojistovna.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String renderIndex()
+    {
+        return "pages/home/index";
+    }
+}
