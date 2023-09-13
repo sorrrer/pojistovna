6d20768936a91da004365133bfd90ae824c946ee
Kopecký Jiří <jukopecky@gmail.com>
2023-09-13 21:55:38 +0200

    vytvoření userDTO

---
+++ src/main/java/cz/kopecky/pojistovna/models/dto/UserDTO.java
@@ -0,0 +1,86 @@
+package cz.kopecky.pojistovna.models.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import javax.swing.*;
+
+public class UserDTO {
+    @NotNull(message = "Vyplňte své jméno.")
+    @NotBlank(message = "Vyplňte své jméno.")
+    private String firstName;
+    @NotBlank(message = "Vyplňte své příjmení.")
+    @NotNull(message = "Vyplňte své příjmení.")
+    private String secondName;
+    @NotBlank(message = "Vyplňte svou adresu.")
+    @NotNull(message = "Vyplňte svou adresu.")
+    private String address;
+    @NotBlank(message = "Vyplňte svou ulici.")
+    @NotNull(message = "vyplňte svou ulici.")
+    private String street;
+    @NotBlank(message = "vyplňte svůj email.")
+    @NotNull(message = "Vyplňte svůj email.")
+    private String email;
+    @NotBlank(message = "Vyplňte své směrovací číslo.")
+    @NotNull(message = "Vyplňte své směrovací číslo")
+    private int psc;
+    @NotBlank(message = "Vyplňte své telefonní číslo.")
+    @NotNull(message = "Vyplňte své telefonní číslo.")
+    private int phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPsc() {
+        return psc;
+    }
+
+    public void setPsc(int psc) {
+        this.psc = psc;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
