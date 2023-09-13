b0c9162de681db5f75fbb30f575098a64ef4843f
Kopecký Jiří <jukopecky@gmail.com>
2023-09-13 21:08:01 +0200

    Založení projektu

---
+++ src/main/java/cz/kopecky/pojistovna/pojistovnaMainApp.java
@@ -0,0 +1,10 @@
+package cz.kopecky.pojistovna;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class pojistovnaMainApp {
+    public static void main(String[] args) {
+
+    }
+}
