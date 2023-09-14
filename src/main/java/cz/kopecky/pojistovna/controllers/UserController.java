63cf97f6b9a0e892060e3bc5825cc2c39353c03d
Kopecký Jiří <jukopecky@gmail.com>
2023-09-14 07:18:52 +0200

    vytvoření create formuláře.

--- src/main/java/cz/kopecky/pojistovna/controllers/UserController.java
+++ src/main/java/cz/kopecky/pojistovna/controllers/UserController.java
@@ -1,7 +1,9 @@
 package cz.kopecky.pojistovna.controllers;
 
+import cz.kopecky.pojistovna.models.dto.UserDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +13,11 @@ public class UserController {
     public String renderUserIndex()
     {
         return "pages/users/index";
+    }
+    @GetMapping("create")
+    public String renderUserForm(@ModelAttribute UserDTO user)
+    {
+        return "pages/users/create";
     }
 
 }
