e457cfb0f3302192ba7df2d27e97d14ae62e31f5
Kopecký Jiří <jukopecky@gmail.com>
2023-09-14 20:03:56 +0200

    implementace userDetails.

--- src/main/java/cz/kopecky/pojistovna/data/entities/PersonEntity.java
+++ src/main/java/cz/kopecky/pojistovna/data/entities/PersonEntity.java
@@ -2,9 +2,15 @@ package cz.kopecky.pojistovna.data.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-public class PersonEntity {
+public class PersonEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long personId;
@@ -33,10 +39,6 @@ public class PersonEntity {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
         this.password = password;
     }
@@ -56,4 +58,43 @@ public class PersonEntity {
     public void setAdmin(boolean admin) {
         this.admin = admin;
     }
+   @Override
+   public String getUsername()
+   {
+       return email;
+   }
+    @Override
+    public String getPassword()
+    {
+        return password;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority>getAuthorities()
+    {
+        SimpleGrantedAuthority authority= new SimpleGrantedAuthority("ROLE_" + (admin?"ADMIN":"USER"));
+        return List.of(authority);
+    }
+    @Override
+    public boolean isAccountNonExpired()
+    {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked()
+    {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired()
+    {
+        return true;
+    }
+    @Override
+    public boolean isEnabled()
+    {
+        return true;
+    }
+
 }
