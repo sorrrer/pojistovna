b81568fff60cd32ee25558965ecb78da951691b2
Kopecký Jiří <jukopecky@gmail.com>
2023-09-14 07:34:46 +0200

    připojení k databázi.

--- src/main/java/cz/kopecky/pojistovna/controllers/UserController.java
+++ src/main/java/cz/kopecky/pojistovna/controllers/UserController.java
@@ -1,23 +1,39 @@
 package cz.kopecky.pojistovna.controllers;
 
 import cz.kopecky.pojistovna.models.dto.UserDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    /* controller pro šablonu users/index */
     @GetMapping
     public String renderUserIndex()
     {
         return "pages/users/index";
     }
+    /* controller pro šablonu users/create */
     @GetMapping("create")
     public String renderUserForm(@ModelAttribute UserDTO user)
     {
         return "pages/users/create";
     }
+    /* controller pro odeslání dat z create šablony */
+    @PostMapping("create")
+    public String createUserForm(@Valid @ModelAttribute UserDTO user,BindingResult result)
+    {
+        if(result.hasErrors())
+            return renderUserForm(user);
+        System.out.println(user.getFirstName() + " " + user.getSecondName()) ;
+        return "redirect:/users";
+
+    }
+
 
 }
--- src/main/java/cz/kopecky/pojistovna/pojistovnaMainApp.java
+++ src/main/java/cz/kopecky/pojistovna/pojistovnaMainApp.java
@@ -2,8 +2,10 @@ package cz.kopecky.pojistovna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class pojistovnaMainApp {
     public static void main(String[] args) {
         SpringApplication.run(pojistovnaMainApp.class, args);
